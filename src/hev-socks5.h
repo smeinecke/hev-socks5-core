f25d44f4c6aff00d909cc787e20f10dbbeaa6574
Stefan Meinecke <meinecke@greensec.de>
2026-04-11 15:41:12 +0200

    Add socks4 support.

--- src/hev-socks5.h
+++ src/hev-socks5.h
@@ -15,6 +15,8 @@
 
 #include <hev-object.h>
 
+#include <hev-socks5-proto.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -52,6 +54,9 @@ struct _HevSocks5
     int udp_associated;
     HevSocks5Type type;
     HevSocks5AddrFamily addr_family;
+    HevSocks5Version version;
+
+    void *data;
 };
 
 struct _HevSocks5Class
